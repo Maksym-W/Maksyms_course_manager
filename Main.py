efdb77f8e90871799b864ce0889c019b7e3728c9
Max <m.woychyshyn@mail.utoronto.ca>
2023-10-13 15:05:35 -0400

    Added function: output_file_data

--- Main.py
+++ Main.py
@@ -5,9 +5,29 @@ def output_file_data(file_name : str) -> None:
     print("They make up: " + f.readline().rstrip() + "% of your final grade.")
     print("They are as follows:\n")
 
+    total_weight = 0
+    grade = 0
+    while True:
+        line = f.readline().rstrip()
+        if line == "":
+            break
+        results = line.split(',')
+        print("Name of Assignment: " + results[2] +
+        ". Weight of the final grade: " + results[1] +
+        "%. Grade: " + results[0] + "%\n")
+        total_weight += int(results[1])
+        grade += (int(results[0]) * (int(results[1])/100))
+
+    print("If you were to get 0% on the rest of your assignments, "
+          "you would have this as your final grade: " + str(grade) + "%.")
+    print("If you were to get 100% on the rest of your assignments, "
+          "you would have this as your final grade: " + str(grade + (100- total_weight)) + "%.")
 
     f.close()
 
+
+def input_file_data(file_name : str, assignment : list) -> None:
+    print("TODO") # TODO
 
 
 def main():
@@ -19,26 +39,13 @@ def main():
     if choice == "1":
         output_file_data("Courses/" + course + ".txt")
     elif choice == "2":
-        f=open("Courses/" + course + ".txt", "w")
-        f.write("This is a test")
-        f.close()
+        assignment = [None, None, None]
+        assignment[2] = input("Enter the name of the assignment: ")
+        assignment[0] = input("Enter your grade: ")
+        assignment[1] = input("Enter the weight of assignment: ")
+        input_file_data("Courses/" + course + ".txt", assignment)
     else:
         print("Please enter in 1 or 2 next time")
 
 
 main() # Lets run the code
-
-"""
-CURRENT TEMPLATE
-
-You currently have: 0 assessments logged
-They make up: 0% of your final grade
-They are as follows:
-\n
-ASSIGNMENTS 1
-\n
-Your current average is: x%
-If you were to get 0% on the rest of your assignments, you would have this as your final grade y%
-If you were to get 100% on the rest of your assignments, you would have this as your final grade y%
-The highest Possible GPA you can get is.
-"""
